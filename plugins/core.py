3d60145b2a13bfa4d8d95e0965fb15d41ad2e684
Anthony Nguyen <anknguyen@gmail.com>
2014-08-26 11:28:28 -0400

    Added basic documentation for each of the events

--- plugins/core.py
+++ plugins/core.py
@@ -12,6 +12,12 @@ class CorePlugin:
         self.bot.registerCommand("reload", self.cmd_reload, True)
         self.bot.registerCommand("die", self.cmd_die, True)
 
+        self.bot.registerEvent("private_message", self.event_private_message)
+        self.bot.registerEvent("public_message", self.event_public_message)
+        self.bot.registerEvent("nick_change", self.event_nick_change)
+        self.bot.registerEvent("user_part", self.event_user_part)
+        self.bot.registerEvent("user_quit", self.event_user_quit)
+
     def shutdown(self):
         pass
 
@@ -20,6 +26,43 @@ class CorePlugin:
     #             Event Handlers               #
     #------------------------------------------#
     """
+
+    def event_private_message(self, ev):
+        """
+        + person who said it: ev.source
+           + their nick: ev.source.nick
+
+        + what they said: ev.arguments[0]
+        """
+        pass
+
+    def event_public_message(self, ev):
+        """
+        + person who said it: ev.source
+           + their nick: ev.source.nick
+
+        + what they said: ev.arguments[0]
+        """
+        pass
+
+    def event_nick_change(self, ev):
+        """
+        + old nick: ev.source.nick
+        + new nick: ev.target
+        """
+        pass
+
+    def event_user_part(self, ev):
+        """
+        + person who left: ev.source.nick
+        """
+        pass
+
+    def event_user_quit(self, ev):
+        """
+        + person who left: ev.source.nick
+        """
+        pass
 
     """
     #------------------------------------------#
