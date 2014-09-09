89c4212be405ba5c50fa55879f275b60e3cc3f06
Anthony Nguyen <anknguyen@gmail.com>
2014-09-09 16:30:23 -0400

    Bumped up version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name ="basebot",
-    version = "1.0.8",
+    version = "1.0.9",
     license = "MIT",
 
     description = "a base IRC bot extensible with plugins",
